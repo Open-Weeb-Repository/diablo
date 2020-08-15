586e3d7d80a081b16d67b29fa08073d43a31d503
aquaswim <bayusetiawan37@gmail.com>
2020-08-15 18:06:49 +0700

    Adding dummy publish method in dummy provider

--- src/providers/anime_dummy_fail/index.ts
+++ src/providers/anime_dummy_fail/index.ts
@@ -1,4 +1,6 @@
 import {Diablo} from "diablo";
+import {IDBProjectProvider} from "project-providers";
+import projectProviderRepo from "../../repositories/project-providers";
 
 export default class implements Diablo.IProjectProvider{
     name = 'anime_dummy';
@@ -11,7 +13,17 @@ export default class implements Diablo.IProjectProvider{
     }
 
     async publish(): Promise<boolean> {
-        return true;
+        let providerInfo: IDBProjectProvider = {
+            extraData: undefined,
+            language: "Dummy",
+            logo: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+            site: "Dummy",
+            title: "Dummy Provider",
+            type: "dummy",
+            workerJobName: this.name
+        };
+        const result = await projectProviderRepo.updateProviders(providerInfo);
+        return Boolean((result as any).ok);
     }
 
 }
